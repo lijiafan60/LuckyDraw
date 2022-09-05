4b49ee011df2756d20b54ddfa6e1b457cfee06ff
李佳帆 <3151342895@qq.com>
2022-09-05 10:21:05 +0800

    initial

---
+++ src/test/java/com/ljf/luckydraw/LuckyDrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljf.luckydraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyDrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
